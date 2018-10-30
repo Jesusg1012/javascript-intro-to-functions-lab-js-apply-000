ae325880e092ed8a89626321579653d1778161b3
jesusg1012 <jesus.jg.medina@gmailcom>
2018-10-30 18:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toUpperCase === string)
   {
-    return ""
+    return "YES INDEED!"
+  }
+  if (string.toLowerCase === string)
+  {
+    return "I can\'t hear you"
   }
 }
